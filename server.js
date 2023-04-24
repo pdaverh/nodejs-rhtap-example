a9ec30cc6164637d448aa1fa33b41a5a245fffc5
Parag Dave <48592473+pdaverh@users.noreply.github.com>
2023-04-24 08:50:43 -0600

    Update server.js

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ app.use(require('pino-http')({logger: pino}));
 app.get('/', (req, res) => {
   // Use req.log (a `pino` instance) to log JSON:
   req.log.info({message: 'Hello Batman! Welcome to our Node.js Starter Application!'});
-  res.send('Hello Superman! Welcome to our Node.js Application!');
+  res.send('Hello Batman! Welcome to our Node.js Application!');
 });
 
 app.get('*', (req, res) => {
